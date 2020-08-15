ec4c146463935fab1a343fd4a1d3d23b27bb3c00
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2020-08-15 18:02:53 -0300

    teste

---
+++ sophietube/android/app/src/main/java/com/rodarte/sophietube/MainActivity.java
@@ -0,0 +1,6 @@
+package com.rodarte.sophietube;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ sophietube/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  title: "Sophietube",
+));
