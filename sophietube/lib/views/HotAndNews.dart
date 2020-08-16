a41bd8fb85e6dfb42977f924739bcb01571e2cd8
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2020-08-15 21:13:09 -0300

    views ongoing

---
+++ sophietube/lib/views/HotAndNews.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HotAndNews extends StatefulWidget {
+  @override
+  _HotAndNewsState createState() => _HotAndNewsState();
+}
+
+class _HotAndNewsState extends State<HotAndNews> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
