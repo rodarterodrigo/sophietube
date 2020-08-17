915721f6f6b31b9c1795426435a08007e9169f54
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2020-08-17 01:27:28 -0300

    SearchDelegate refactor

--- sophietube/lib/search/CustomSearchDelegate.dart
+++ sophietube/lib/search/CustomSearchDelegate.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sophietube/api/SugestionApi.dart';
 import 'package:sophietube/models/Sugestion.dart';
+import 'package:sophietube/search/SearchHelper.dart';
 
 class CustomSearchDelegate extends SearchDelegate<String>{
   @override
@@ -16,7 +17,6 @@ class CustomSearchDelegate extends SearchDelegate<String>{
         },
       ),
     ];
-
 
   @override
   Widget buildLeading(BuildContext context)
@@ -35,92 +35,20 @@ class CustomSearchDelegate extends SearchDelegate<String>{
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    SearchHelper searchHelper = new SearchHelper();
     if(query.isNotEmpty){
-//      List<String> list = List();
+
       SugestionApi api = SugestionApi();
       Future<List<Sugestion>> _listSugestions(String search) async => await api.search(search);
-//      list = [
-//        "Android", "Navegação", "IOS", "Flutter"
-//      ].where((element) => element.toLowerCase().startsWith(query.toLowerCase())).toList();
+
       return FutureBuilder<List<Sugestion>>(
         future: _listSugestions(query),
-        builder: (context, snapshot){
-          switch(snapshot.connectionState){
-            case ConnectionState.none:
-              return Center(
-                child: Text(
-                  "Verifique sua conexão.",
-                  style: TextStyle(
-                      fontSize: 20
-                  ),
-                ),
-              );
-            case ConnectionState.waiting:
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-              break;
-            case ConnectionState.active:
-            case ConnectionState.done:
-              if(snapshot.hasData){
-                return ListView.separated(
-                  itemBuilder: (context, ind){
-                    List<Sugestion> sugestions = snapshot.data;
-                    String title = sugestions[ind].title;
-                    return Column(
-                      children: <Widget>[
-                        ListTile(
-                          onTap: () => close(context, title),
-                          title: Text(title),
-                        ),
-                      ],
-                    );
-                  },
-                  separatorBuilder: (context, ind) => Divider(
-                    height: 2,
-                    color: Colors.grey,
-                  ),
-                  itemCount: snapshot.data.length,
-                );
-              }
-              else{
-                return Center(
-                  child: Text(
-                    "Não foi possível retornar dados.",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                );
-              }
-              break;
-          }
-          return Container();
-        },
+        builder: (context, snapshot) => searchHelper.helpSnapShot(context, snapshot)
       );
-//      return ListView.builder(
-//          itemBuilder: (context, ind){
-//            return ListTile(
-//              onTap: (){
-//                close(context, list[ind]);
-//              },
-//              title: Text(
-//                list[ind],
-//              ),
-//            );
-//          },
-//        itemCount: list.length,
-//      );
     }
-    else{
-      return Center(
-        child: Text(
-          "Nenhum resultado para a pesquisa.",
-          style: TextStyle(
-            fontSize: 20
-          ),
-        ),
-      );
+    else
+    {
+      return searchHelper.noResult();
     }
   }
 }
---
+++ sophietube/lib/search/SearchHelper.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:sophietube/models/Sugestion.dart';
+
+class SearchHelper implements SearchDelegate{
+
+
+  String _verifyConection = "Verifique sua conexão.";
+  String _dataFail = "Não foi possível retornar dados.";
+  String _noResult = "Nenhum resultado para a pesquisa.";
+
+  Widget noResult(){
+    return Center(
+      child: Text(
+        _noResult,
+        style: TextStyle(
+            fontSize: 20
+        ),
+      ),
+    );
+  }
+
+  Widget helpSnapShot(BuildContext context, AsyncSnapshot snapshot) {
+    switch (snapshot.connectionState) {
+      case ConnectionState.none:
+        return Center(
+          child: Text(
+            _verifyConection,
+            style: TextStyle(
+                fontSize: 20
+            ),
+          ),
+        );
+      case ConnectionState.waiting:
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+        break;
+      case ConnectionState.active:
+      case ConnectionState.done:
+        if (snapshot.hasData) {
+          return ListView.separated(
+            itemBuilder: (context, ind) {
+              List<Sugestion> sugestions = snapshot.data;
+              String title = sugestions[ind].title;
+              return Column(
+                children: <Widget>[
+                  ListTile(
+                    onTap: () => close(context, title),
+                    title: Text(title),
+                  ),
+                ],
+              );
+            },
+            separatorBuilder: (context, ind) =>
+                Divider(
+                  height: 2,
+                  color: Colors.grey,
+                ),
+            itemCount: snapshot.data.length,
+          );
+        }
+        else {
+          return Center(
+            child: Text(
+              _dataFail,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          );
+        }
+        break;
+    }
+  }
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
